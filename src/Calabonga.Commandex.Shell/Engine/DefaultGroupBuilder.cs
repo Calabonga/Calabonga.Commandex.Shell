dd9713384e704e0caca64ff34e7e1b80d8bb5391
Sergei Calabonga <dev@calabonga.net>
2024-09-18 06:53:42 +0300

    hierarchy for commands grouping created

--- src/Calabonga.Commandex.Shell/Engine/DefaultGroupBuilder.cs
+++ src/Calabonga.Commandex.Shell/Engine/DefaultGroupBuilder.cs
@@ -19,12 +19,18 @@ public class DefaultGroupBuilder : IGroupBuilder
     /// <returns></returns>
     public List<CommandGroup> GetGroups() =>
     [
-        new() { Name = "Commands with HttpClient", Tags = ["http"], Description = "Commands with HttpClient using for request to remote services (API)" },
         new() { Name = "Empty commands type", Tags = ["empty"], Description = "Commands that is not result returned to Shell" },
-        new() { Name = "Commands with result", Tags = ["result"], Description = "Commands with some result for the Shell" },
-        new() { Name = "Commands open Dialog", Tags = ["dialog"], Description = "Commands that run into modal dialog window of the Shell" },
+        new()
+        {
+            Name = "Commands with Result", Tags = ["result"], Description = "Commands with some result for the Shell",
+            SubGroups = [new() { Name = "Commands with HttpClient Result", Tags = ["http"], Description = "Commands with HttpClient using for request to remote services (API)" }]
+        },
+        new()
+        {
+            Name = "Commands open Dialog", Tags = ["dialog"], Description = "Commands that run into modal dialog window of the Shell",
+            SubGroups = [new() { Name = "Wizard commands type", Tags = ["wizard"], Description = "Commands that in modal dialog shows a few steps in the wizard." }]
+        },
         new() { Name = "Command for Person entity", Tags = ["person"], Description = "Command for Person entity management" },
-        new() { Name = "Wizard commands type", Tags = ["wizard"], Description = "Commands that in modal dialog shows a few steps in the wizard." },
         new() { Name = "Command with sharing the same generic Parameter", Tags = ["parameter"], Description = "Command for Person entity management" }
     ];
 }
