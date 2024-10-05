9fa31522f47d5b0d1893bba9afe9bf25d2919517
Sergei Calabonga <dev@calabonga.net>
2024-10-05 09:03:04 +0700

    CommandexData entity created for storage for OAuth2.0 data

---
+++ src/Calabonga.Commandex.Shell/Models/CommandexData.cs
@@ -0,0 +1,18 @@
+﻿using Calabonga.Commandex.Shell.Infrastructure.Security;
+
+namespace Calabonga.Commandex.Shell.Models;
+
+/// <summary>
+/// Commandex Storage Data
+/// </summary>
+public class CommandexData
+{
+    /// <summary>
+    /// Who is data for
+    /// </summary>
+    public string Username { get; set; } = null!;
+    /// <summary>
+    /// Personal data
+    /// </summary>
+    public SecureData Data { get; set; } = null!;
+}
