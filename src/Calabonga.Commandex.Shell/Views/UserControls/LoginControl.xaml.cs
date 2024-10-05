4b4f6cfaf0ae85a46d067757c9ae1fa41ebf6f6b
Sergei Calabonga <dev@calabonga.net>
2024-10-05 08:56:14 +0700

    LoginControl created

---
+++ src/Calabonga.Commandex.Shell/Views/UserControls/LoginControl.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Calabonga.Commandex.Shell.ViewModels.UserControls;
+using Microsoft.Extensions.DependencyInjection;
+using System.Windows.Controls;
+
+namespace Calabonga.Commandex.Shell.Views.UserControls
+{
+    /// <summary>
+    /// Interaction logic for LoginControl.xaml
+    /// </summary>
+    public partial class LoginControl : UserControl
+    {
+        public LoginControl()
+        {
+            InitializeComponent();
+            DataContext = App.Current.Services.GetService<LoginControlViewModel>();
+        }
+    }
+}
