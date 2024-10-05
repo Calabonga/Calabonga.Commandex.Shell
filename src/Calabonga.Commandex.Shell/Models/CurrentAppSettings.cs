a683bf720ffb31f4a5b8ac9bf8701bcb89ca3e3f
Sergei Calabonga <dev@calabonga.net>
2024-10-05 09:03:26 +0700

    CurrentAppSettings updated with new properties

--- src/Calabonga.Commandex.Shell/Models/CurrentAppSettings.cs
+++ src/Calabonga.Commandex.Shell/Models/CurrentAppSettings.cs
@@ -9,6 +9,26 @@ public class CurrentAppSettings : AppSettings
     /// </summary>
     public string DefaultViewName { get; init; } = null!;
 
+    /// <summary>
+    /// Authorization Server URL (OAuth2.0)
+    /// </summary>
+    public string? AuthorizationServerUrl { get; set; }
+
+    /// <summary>
+    /// OAuth2.0 Client Identity
+    /// </summary>
+    public string? ClientId { get; set; }
+
+    /// <summary>
+    /// OAuth2.0 Client Secret
+    /// </summary>
+    public string? ClientSecret { get; set; }
+
+    /// <summary>
+    /// OAuth2.0 Grand Type
+    /// </summary>
+    public string? GrantType { get; set; }
+
     public static string GetViewResourceName(string settingsName)
     {
         Enum.TryParse<CommandViewType>(settingsName, true, out var defaultList);
